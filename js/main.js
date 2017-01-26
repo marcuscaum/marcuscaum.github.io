c159682a573cf83cce52cd5854c360662f7b0ad4
marcuscaum <marcuslucas.jb@gmail.com>
2017-01-26 17:07:34 -0200

    ball maker

--- js/main.js
+++ js/main.js
@@ -1,12 +1,48 @@
 var Main = (function() {
 
   function init() {
-    $(".element").typed({
-      strings: ["First sentence.", "Second sentence."],
-      typeSpeed: 0
+    $(document).on('keypress', function() {
+      createDiv();
+    })
+  }
+
+  function createDiv() {
+    $ball = $('<div class="ball"></div>').css({
+      'left': randomAttributes.width(),
+      'top': randomAttributes.height(),
+      'backgroundColor': randomAttributes.color()
+    });
+
+
+    $ball.appendTo('body').delay(500).fadeIn(100, function () {
+      $(this).fadeOut(500, function() {
+        $(this).remove();
+      });
     });
   }
 
+  var randomAttributes = {
+
+    BALL_SIZE: 100,
+
+    width: function () {
+      return (Math.random() * ($(window).width() - this.BALL_SIZE)).toFixed() + 'px';
+    },
+
+    height: function () {
+      return (Math.random() * ($(window).height() - this.BALL_SIZE)).toFixed() + 'px';
+    },
+
+    color: function () {
+      var rgb = [];
+
+      for(var i = 0; i < 3; i++) {
+        rgb.push(Math.floor(Math.random() * 255));
+      }
+
+      return 'rgb('+ rgb.join(',') +')';
+    }
+  }
 
   return {
     init: init
